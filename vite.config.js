5011b0c438c1225ee950d6be12edc115b61869f6
pasdevs <akmaluddinfadhilah@gmail.com>
2026-04-07 10:46:13 +0700

    update node version, add sri, and delete .env

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import sri from 'vite-plugin-sri';
 
 export default defineConfig({
   base: "/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    sri()
+  ],
   build: {
     chunkSizeWarningLimit: 1000, // naikkan jadi 1MB
+    outDir: "dist",
+    sourcemap: false
   },
 });
